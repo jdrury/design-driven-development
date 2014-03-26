94f888a0e47b5cec84fc5dbc9539099939805602
James Drury <jdrury86@gmail.com>
2014-03-26 11:56:24 -0400

    uprunes user auth to github and local only

--- config/env/test.js
+++ config/env/test.js
@@ -1,34 +1,14 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/test-driven-design-test',
+	db: 'mongodb://localhost/3DT-test',
 	port: 3001,
 	app: {
-		name: 'Test-driven Design'
-	},
-	facebook: {
-		clientID: '1409189336005712',
-		clientSecret: 'ac9a4401257ce2cd2f0d09ecd429d592',
-		callbackURL: 'http://localhost:3000/auth/facebook/callback'
-	},
-	twitter: {
-		clientID: 'ZGWED1alGOr29ouhzIuesg',
-		clientSecret: 'mTUdpqlwzHFFO1jelcuyTR1RaZ07nlX7C89lWx3Vleo',
-		callbackURL: 'http://localhost:3000/auth/twitter/callback'
+		name: 'Design Driven Development Tool'
 	},
 	github: {
-		clientID: '67a901efc7bf145c7b66',
-		clientSecret: 'cd4d7f4c8de133df97d5b112553c31378758488b',
+		clientID: '0ac83f6b151c2da78fdc',
+		clientSecret: '72563dc41779974b6ae776f8c4a0eb25a5ebde70',
 		callbackURL: 'http://localhost:3000/auth/github/callback'
-	},
-	google: {
-		clientID: '72368565161.apps.googleusercontent.com',
-		clientSecret: 'qyNr2gK_zS49zdG5ArYj8Myt',
-		callbackURL: 'http://localhost:3000/auth/google/callback'
-	},
-	linkedin: {
-		clientID: '775yte5qtyc417',
-		clientSecret: 'BC7qO6u7ZyztQkxS',
-		callbackURL: 'http://localhost:3000/auth/linkedin/callback'
 	}
 };
