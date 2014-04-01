3bd623c94c370f8f4e4779358a3a97f9185cf1fb
root <root@3dt.(none)>
2014-04-01 16:50:04 -0400

    setup

---
+++ setup_phantom.sh
@@ -0,0 +1,3 @@
+:#!/bin/bash
+mv ./node_modules/webshot/node_modules/phantomjs/lib/phantom/bin/phantomjs ./node_modules/webshot/node_modules/phantomjs/lib/phantom/bin/phantomjs-bak
+cp ./phantomjs-linux ./node_modules/webshot/node_modules/phantomjs/lib/phantom/bin/phantomjs
