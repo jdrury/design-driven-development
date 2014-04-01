3a9de51ce8346526071f742c7c018b5c43692ea6
Lauren Ashpole <lauren@laurenashpole.com>
2014-04-01 19:58:07 -0400

    fixed github callback and upload button

--- config/env/test.js
+++ config/env/test.js
@@ -9,6 +9,6 @@ module.exports = {
 	github: {
 		clientID: '0ac83f6b151c2da78fdc',
 		clientSecret: '72563dc41779974b6ae776f8c4a0eb25a5ebde70',
-		callbackURL: 'http://localhost:3000/auth/github/callback'
+		callbackURL: 'http://designdriven.fullstackacademy.com/auth/github/callback'
 	}
 };
