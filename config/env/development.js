7c3d4944bafd2e14471bfdadb1bcc4c5f0839130
CoderCookE <ejcook111@gmail.com>
2014-04-02 11:45:45 -0400

    changed callback

--- config/env/development.js
+++ config/env/development.js
@@ -8,6 +8,6 @@ module.exports = {
 	github: {
     clientID: '0ac83f6b151c2da78fdc',
     clientSecret: '72563dc41779974b6ae776f8c4a0eb25a5ebde70',
-    callbackURL: 'http://designdriven.fullstackacademy.com/auth/github/callback'
+    callbackURL: 'http://localhost:3000/auth/github/callback'
   }
 };
